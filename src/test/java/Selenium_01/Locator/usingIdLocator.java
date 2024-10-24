0f6d1502af4e1603594e845b1ac906904ebca58a
mustafamandiraci <mustafarya@yahoo.co.uk>
2024-10-24 19:43:00 -0400

    activity 1

--- src/test/java/Selenium_01/Locator/usingIdLocator.java
+++ src/test/java/Selenium_01/Locator/usingIdLocator.java
@@ -5,14 +5,25 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class usingIdLocator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
          driver.get("https://retail.tekschool-students.com/");
-         driver.manage().window().maximize();
 
+
+         //Step 1 Locate the Element
          By signInlink = By.id("signinLink");
 
+        // Step 2 asking Selenium to Find the Element
         WebElement signInElement =driver.findElement(signInlink);
+
+
+
+        //Step 3 Perform the action
         signInElement.click();
+        Thread.sleep(10_000);
+
+
+        driver.quit();
     }
 }
